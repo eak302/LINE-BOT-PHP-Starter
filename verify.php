94f7df4da5e6f9b08cde1e4ca4e5cb4c33a1404c
eak302 <eak302@gmail.com>
2016-10-12 18:40:52 +0700

    edit verify.php

--- verify.php
+++ verify.php
@@ -1,11 +1,11 @@
 <?php
     $access_token = 'YAZLaG6TkebnciInDS/+n+X6Jxl8ayLKt2t1kIHgQk/mFpSogb0YiGT9DmHAONWW/gG5hBnybtx81RK1IUqtQ/31RevYZamcmwaj2Euy79krNI/Nr4gqPxx56AshWrpAvfXWRuKWuyADO0+glVrZngdB04t89/1O/w1cDnyilFU=';
     $proxy = 'http://fixie:GuO3U3SWaIqzcI6@velodrome.usefixie.com:80';
-    $proxyauth = 'eak302:abcd028623112';
+    $proxyauth = 'eak302@gmail.com:028623112';
 
     $url = 'https://api.line.me/v1/oauth/verify';
 
-    $headers = array('Authorization: Bearer ' , $access_token);
+    $headers = array('Authorization: Bearer ' . $access_token);
 
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
