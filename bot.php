98bb05cc2448c6fe8493004d970bbbace891bbd8
eak302 <eak302@gmail.com>
2016-10-13 17:40:43 +0700

    add php

--- bot.php
+++ bot.php
@@ -1,3 +1,3 @@
 <?php
 
-    header("http://dentsu360.net/demo/temp/save.php?replytoken=test&type=message");
+    header("location: http://dentsu360.net/demo/temp/save.php?replytoken=test&type=message");
