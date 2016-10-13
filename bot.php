b9852205741c48c827d6a2ecbbbac00b66572bf0
eak302 <eak302@gmail.com>
2016-10-13 15:20:13 +0700

    test

--- bot.php
+++ bot.php
@@ -13,12 +13,12 @@
     //$content = $_POST;
     $arr = array(
         'events'=>array([
-            'replyToken': 'nHuyWiB7yP5Zw52FIkcQobQuGDXCTA',
+            'replyToken'=>'nHuyWiB7yP5Zw52FIkcQobQuGDXCTA',
             'type'=>'message',
             'timestamp'=>1462629479859,
-            'source'=>(
-                'type': 'user',
-                'userId': 'U206d25c2ea6bd87c17655609a1c37cb8'
+            'source'=>array(
+                'type'=>'user',
+                'userId'=>'U206d25c2ea6bd87c17655609a1c37cb8'
             ),
             'message'=>array(
                 'id'=>'325708',
@@ -73,7 +73,40 @@
     {
         foreach ( $events['events'] as $event )
         {
-            
+            // Reply only when message sent is in 'text' format
+            if ($event['type'] == 'message' && $event['message']['type'] == 'text')
+            {
+                // Get text sent
+                $text = $event['message']['text'];
+                // Get replyToken
+                $replyToken = $event['replyToken'];
+
+                // Build message to reply back
+                $messages = [
+                    'type' => 'text',
+                    'text' => $text
+                ];
+
+                // Make a POST Request to Messaging API to reply to sender
+                $url = 'https://api.line.me/v2/bot/message/reply';
+                $data = [
+                    'replyToken' => $replyToken,
+                    'messages' => [$messages],
+                ];
+                $post = json_encode($data);
+                $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                $result = curl_exec($ch);
+                curl_close($ch);
+
+                echo $result . "\r\n";
+            }
         }
     }
     
