641bb377b1fd470c4a1c0260afc45913dc126b68
eak302 <eak302@gmail.com>
2016-10-13 17:38:57 +0700

    add php

--- temp.php
+++ temp.php
@@ -57,29 +57,7 @@ Refresh token
 pFg7tNFsnO5yfGV8Mvdq
 
 
-curl -X GET \
--H 'Authorization: Bearer {hhuUL3LjBSyrk2Wz5UZLrSHjX2hlB+rbC59sQUiu3P+viXg8T1FSNp8eE3guCCAU6Z/AA+x3zGJ0z/XOqUiYD4OZtGEfSJS5RrZ9BqkcNRcIQ3HCMwUFYE+fxEmgo8MtQCsFDTiepiR17PS8F07kFLY0uH38g5VIshXuw0uh2S4=}' \
-https://api.line.me/v1/profile
-
 POST /v1/events HTTP/1.1
 Host: api.line.me
 Content-type: application/json; charset=UTF-8
 X-LINE-ChannelToken: hhuUL3LjBSyrk2Wz5UZLrSHjX2hlB+rbC59sQUiu3P+viXg8T1FSNp8eE3guCCAU6Z/AA+x3zGJ0z/XOqUiYD4OZtGEfSJS5RrZ9BqkcNRcIQ3HCMwUFYE+fxEmgo8MtQCsFDTiepiR17PS8F07kFLY0uH38g5VIshXuw0uh2S4=
-
-
-curl -X POST \
--H 'Content-Type:application/json' \
--H 'Authorization: Bearer {YAZLaG6TkebnciInDS/+n+X6Jxl8ayLKt2t1kIHgQk/mFpSogb0YiGT9DmHAONWW/gG5hBnybtx81RK1IUqtQ/31RevYZamcmwaj2Euy79krNI/Nr4gqPxx56AshWrpAvfXWRuKWuyADO0+glVrZngdB04t89/1O/w1cDnyilFU=}' \
--d '{
-    "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-    "messages":[
-        {
-            "type":"text",
-            "text":"Hello, user"
-        },
-        {
-            "type":"text",
-            "text":"May I help you?"
-        }
-    ]
-}' https://api.line.me/v2/bot/message/reply
