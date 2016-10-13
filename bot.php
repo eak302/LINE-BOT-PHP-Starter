afc80550331729592c279a614918109f23ecff74
eak302 <eak302@gmail.com>
2016-10-13 13:34:14 +0700

    test

--- bot.php
+++ bot.php
@@ -40,6 +40,8 @@
             
             //var_dump($events);
             //exit();
+            echo $event['type'];
+            exit();
             
             // Reply only when message sent is in 'text' format
             if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
