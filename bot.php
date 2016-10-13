0820b73d1ead6848d69d14904414e0afcddde153
eak302 <eak302@gmail.com>
2016-10-13 13:45:28 +0700

    test

--- bot.php
+++ bot.php
@@ -40,7 +40,7 @@
             
             //var_dump($events);
             //exit();
-            print_r($events);
+            var_dump($events);
             exit();
             
             // Reply only when message sent is in 'text' format
