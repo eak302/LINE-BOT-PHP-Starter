0c438a913b7a21d1fc6f8aef4c8b4c98a786d5ff
eak302 <eak302@gmail.com>
2016-10-13 14:25:41 +0700

    test

--- bot.php
+++ bot.php
@@ -33,6 +33,40 @@
         if ($events['events']['type'] == 'message' && $events['events']['message']['type'] == 'text')
         {
             echo 'true';
+            
+            
+            // Get text sent
+            $text = $events['events']['message']['text'];
+            // Get replyToken
+            $replyToken = $events['events']['replyToken'];
+
+            // Build message to reply back
+            $messages = [
+                'type' => 'text',
+                'text' => $text
+            ];
+
+                // Make a POST Request to Messaging API to reply to sender
+                $url = 'https://api.line.me/v2/bot/message/reply';
+                $data = [
+                    'replyToken' => $replyToken,
+                    'messages' => [$messages],
+                ];
+                $post = json_encode($data);
+                $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                curl_setopt($ch, CURLOPT_PROXY, $proxy);
+                curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+                $result = curl_exec($ch);
+                curl_close($ch);
+
+                echo $result . "\r\n";
         }
     }
 
